4f6f02a8ce4b083842e288ce9fff6d2491663160
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-09 16:07:12 -0700

    done till step 10

--- MultiplyChallenge/ViewController.m
+++ MultiplyChallenge/ViewController.m
@@ -1,27 +1,67 @@
-//
-//  ViewController.m
-//  MultiplyChallenge
-//
-//  Created by Ronald Hernandez on 3/9/15.
-//  Copyright (c) 2015 Ron. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UILabel *multiplierLabel;
+@property (weak, nonatomic) IBOutlet UISlider *theSlider;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *operatorSegmentControl;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.theSlider.minimumValue = 0;
+    self.theSlider.maximumValue = 10;
+//    self.multiplierLabel.text = [NSString stringWithFormat:@"%1.2f", self.theSlider.value];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)calculateButtonPressed:(id)sender {
+
+    float inputAmount = [self.numberTextField.text floatValue];
+    float multiplyAmount = [self.multiplierLabel.text floatValue];
+    float finalAmount;
+
+    if (self.operatorSegmentControl.selectedSegmentIndex == 0){
+        finalAmount = inputAmount * multiplyAmount;
+    }else if (self.operatorSegmentControl.selectedSegmentIndex ==1){
+        finalAmount = inputAmount / multiplyAmount;
+    }
+
+
+    if (finalAmount > 20){
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+    else {
+        self.view.backgroundColor = [UIColor whiteColor];
+    }
+
+    int finalAmountTemp = (int) finalAmount;
+
+    if (finalAmountTemp % 3 == 0 && finalAmountTemp % 5 == 0){
+        self.answerLabel.text = @"fizzbuzz";
+    }
+    else if (finalAmountTemp % 3 == 0){
+        self.answerLabel.text = @"fizz";
+    }
+    else if (finalAmountTemp % 5 == 0){
+        self.answerLabel.text = @"buzz";
+    }
+    else {
+        self.answerLabel.text = [NSString stringWithFormat:@"%1.2f", finalAmount];
+    }
+}
+- (IBAction)sliderAction:(id)sender {
+
+    int temp = (int)self.theSlider.value;
+
+    self.multiplierLabel.text = [NSString stringWithFormat:@"%d", temp];
+}
+- (IBAction)userHitEnterButttonDismissKeyboard:(id)sender {
+
+    [self.numberTextField resignFirstResponder];
 }
 
 @end
